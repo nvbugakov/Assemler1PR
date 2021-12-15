310d41f3469b6105c4030656a4a2952f7a99c218
nvbugakov <nvbugakov@mail.ru>
2021-12-15 13:15:12 +0300

    ass1pr

---
+++ ass1pr/ass1pr/ass1pr.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    char s[256];
+    char f[] = "%s";
+    char fd[] = "%d\n";
+    _asm {
+        
+        lea ebx, s       
+        push ebx       
+        lea ecx, f       
+        push ecx        
+        call scanf       
+        add esp, 8        
+        mov ecx, -1        
+        dec ebx       
+        b1 :
+        inc ecx   
+            inc ebx
+            mov al, [ebx]            
+            cmp al, 0            
+            jnz b1           
+            push ecx           
+            lea ebx, fd           
+            push ebx            
+            call printf            
+            add esp, 8
+    }
+}
+
+
+
